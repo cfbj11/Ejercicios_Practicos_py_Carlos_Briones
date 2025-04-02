b5967459b47eec0740e79b1a9fa0610b266fcdf8
cfbj11 <cfbriones@uamv.edu.ni>
2025-04-02 14:36:55 -0600

    Busqueda Binaria

--- Ej3_Alg_Busqueda/models/busqueda_binaria.py
+++ Ej3_Alg_Busqueda/models/busqueda_binaria.py
@@ -1,2 +1,15 @@
 #   BUSQUEDA BINARIA
 
+def busqueda_binaria(lista, objetivo):
+    inicio = 0
+    fin = len(lista) - 1
+
+    while inicio <= fin:
+        medio = (inicio + fin) // 2
+        if lista[medio] == objetivo:
+            return medio
+        elif lista[medio] < objetivo:
+            inicio = medio + 1
+        else:
+            fin = medio - 1
+    return -1
