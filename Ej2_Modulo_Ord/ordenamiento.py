3a64564069518b3380cbd29d996cf46e29cc2e15
cfbj11 <cfbriones@uamv.edu.ni>
2025-04-02 14:01:16 -0600

    Modulo de ordenamiento

--- Ej2_Modulo_Ord/ordenamiento.py
+++ Ej2_Modulo_Ord/ordenamiento.py
@@ -1,2 +1,13 @@
 #   EJERCICIO 2: MODULO DE ORDENAMIENTO
 
+import bubble_sort as bs
+
+def main():
+    lista = [47, 28, 59, 18, 8, 34, 52, 78, 96, 11, 40, 67, 89, 21, 43]
+    print("Lista original:", lista)
+
+    lista_ordenada = bs.bubble_sort(lista)
+    print("Lista ordenada:", lista_ordenada)
+
+if __name__ == "__main__":
+    main()
